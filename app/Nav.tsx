f65f6ffa75cc8485acfaf2a2b23cf3766a3cd609
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-03-23 20:38:56 +0100

    Render Login component if user is not logged in

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -1,11 +1,20 @@
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../pages/api/auth/[...nextauth]';
+import Login from './auth/Login';
 import Link from 'next/link';
 
-export default function Nav() {
+export default async function Nav() {
+  const session = await getServerSession(authOptions);
+
   return (
-    <nav>
+    <nav className="flex justify-between items-center py-8">
       <Link href={'/'}>
         <h1 className="font-bold text-lg">PublishIt.</h1>
       </Link>
+      <ul className="flex items-center gap-6">
+        {!session?.user && <Login />}
+        {session?.user && <h1>{session.user.name}</h1>}
+      </ul>
     </nav>
   );
 }
