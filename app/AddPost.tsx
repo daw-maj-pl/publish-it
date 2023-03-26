4e4b5a538bd923177b72f153217ceb2a611b1c20
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-03-26 16:43:00 +0200

    Add ability to create post by making post request

--- app/AddPost.tsx
+++ app/AddPost.tsx
@@ -1,13 +1,44 @@
 'use client';
 
 import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
+import toast from 'react-hot-toast';
 
 export default function AddPost() {
   const [title, setTitle] = useState('');
   const [isDisabled, setIsDisabled] = useState(false);
+  let toastPostID = 'toastPostID';
+
+  //Create a post
+  const { mutate } = useMutation(
+    async (title: string) =>
+      await axios.post('/api/posts/addPost', {
+        title
+      }),
+    {
+      onError: error => {
+        if (error instanceof AxiosError) {
+          toast.error(error?.response?.data.message, { id: toastPostID });
+        }
+        setIsDisabled(false);
+      },
+      onSuccess: data => {
+        toast.success('Post has been made 🔥', { id: toastPostID });
+        setTitle('');
+        setIsDisabled(false);
+      }
+    }
+  );
+  const submitPost = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsDisabled(true);
+    toastPostID = toast.loading('Creating your post', { id: toastPostID });
+    mutate(title);
+  };
 
   return (
-    <form className="bg-white my-8 p-8 rounded-md">
+    <form onSubmit={submitPost} className="bg-white my-8 p-8 rounded-md">
       <div className="flex flex-col my-4">
         <textarea
           onChange={e => setTitle(e.target.value)}
