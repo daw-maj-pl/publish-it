bec88280cc90109fa3ce10ea46d7c8e3ed7521b2
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-04-06 20:44:53 +0200

    Create PostDetail component

---
+++ app/post/[slug]/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Post from '../../Post';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+type URL = {
+  params: {
+    slug: string;
+  };
+};
+
+const fetchDetails = async (slug: string) => {
+  const response = await axios.get(`/api/posts/${slug}`);
+  return response.data;
+};
+
+export default function PostDetail(url: URL) {
+  const { data, isLoading } = useQuery({
+    queryKey: ['detail-post'],
+    queryFn: () => fetchDetails(url.params.slug)
+  });
+  if (isLoading) return 'Loading.....';
+
+  return (
+    <div>
+      <Post
+        id={data?.id}
+        name={data?.user.name}
+        avatar={data?.user.image}
+        postTitle={data?.title}
+        comments={data?.comments}
+      />
+    </div>
+  );
+}
