96276f92e9470c5e67a41c18d05ad047ed257f62
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-04-08 10:14:16 +0200

    Render comments on the screen

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Post from '../../Post';
+import Image from 'next/image';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import AddComment from '../../AddComment';
@@ -33,6 +34,24 @@ export default function PostDetail(url: URL) {
         comments={data?.comments}
       />
       <AddComment id={data?.id} />
+      {data?.comments?.map(comment => (
+        <div key={comment.id} className="my-6 bg-white p-8 rounded-md">
+          <div className="flex items-center gap-2">
+            <Image
+              width={24}
+              height={24}
+              src={comment.user?.image}
+              alt="avatar"
+              className="rounded-full"
+            />
+            <h3 className="font-bold">{comment?.user?.name}</h3>
+            <h2 className="text-sm">
+              {new Date(comment.createdAt).toLocaleString()}
+            </h2>
+          </div>
+          <div className="py-4">{comment.message}</div>
+        </div>
+      ))}
     </div>
   );
 }
